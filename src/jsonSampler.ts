c1e4ca484dc8d7fa3f870ed61933cdbd4de7fbfb
Felix Bade <flix.bade@gmail.com>
2025-06-15 18:43:40 +0300

    cubic interpolation

--- src/jsonSampler.ts
+++ src/jsonSampler.ts
@@ -3,6 +3,8 @@ interface ParameterSegment {
     endTime: number;
     startValue: number;
     endValue: number;
+    startSpeed?: number;
+    endSpeed?: number;
 }
 
 export class JSONSampler {
@@ -21,20 +23,42 @@ export class JSONSampler {
             // time < startTime of first segment
             if (time < parameter[0].startTime) {
                 values[key] = parameter[0].startValue;
+                if (parameter[0].startSpeed) {
+                    values[key] -= parameter[0].startSpeed * (parameter[0].startTime - time);
+                }
                 continue;
             }
 
             // time > endTime of last segment
-            if (time > parameter[parameter.length - 1].endTime) {
-                values[key] = parameter[parameter.length - 1].endValue;
+            const lastSegment = parameter[parameter.length - 1];
+            if (time > lastSegment.endTime) {
+                values[key] = lastSegment.endValue;
+                if (lastSegment.endSpeed) {
+                    values[key] += lastSegment.endSpeed * (time - lastSegment.endTime);
+                }
                 continue;
             }
 
             // time is between two segments
             const segment = parameter.find((p: ParameterSegment) => time >= p.startTime && time <= p.endTime);
             if (segment) {
-                const t = (time - segment.startTime) / (segment.endTime - segment.startTime);
-                values[key] = segment.startValue + (segment.endValue - segment.startValue) * t;
+                const duration = segment.endTime - segment.startTime;
+                const t = (time - segment.startTime) / duration;
+
+                const p0 = segment.startValue;
+                const p3 = segment.endValue;
+                const p1 = segment.startSpeed !== undefined
+                    ? p0 + segment.startSpeed * duration / 3
+                    : p0 + (p3 - p0) / 3;
+                const p2 = segment.endSpeed !== undefined
+                    ? p3 - segment.endSpeed * duration / 3
+                    : p3 - (p3 - p0) / 3;
+
+                const oneMinusT = 1 - t;
+                values[key] = p0 * oneMinusT * oneMinusT * oneMinusT +
+                    p1 * 3 * t * oneMinusT * oneMinusT +
+                    p2 * 3 * t * t * oneMinusT +
+                    p3 * t * t * t;
                 continue;
             }
 
