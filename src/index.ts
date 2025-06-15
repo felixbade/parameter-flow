7c0c31b86ed92e53cb833772388c67ae2e3c0aec
Felix Bade <flix.bade@gmail.com>
2025-06-15 19:08:33 +0300

    rename file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Player } from './player';
 import { TimelinePlayer } from './timelinePlayer';
-import { PFAnimation } from './jsonSampler';
+import { PFAnimation } from './pfAnimation';
 
 export { Player, TimelinePlayer, PFAnimation };
