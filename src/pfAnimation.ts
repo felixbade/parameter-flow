a0f92962ae77bda3de85bf106e4ae1cf9d597381
Felix Bade <flix.bade@gmail.com>
2025-06-15 21:53:38 +0300

    simplify dataformat

--- src/pfAnimation.ts
+++ src/pfAnimation.ts
@@ -1,16 +1,13 @@
-interface ParameterSegment {
-    startTime: number;
-    endTime: number;
-    startValue: number;
-    endValue: number;
-    startSpeed?: number;
-    endSpeed?: number;
+interface ParameterKeyframe {
+    time: number;
+    value: number;
+    speed?: number;
 }
 
 export class PFAnimation {
-    private _parameters: Record<string, ParameterSegment[]>;
+    private _parameters: Record<string, ParameterKeyframe[]>;
 
-    constructor(public parameters: Record<string, ParameterSegment[]>) {
+    constructor(public parameters: Record<string, ParameterKeyframe[]>) {
         this._parameters = parameters;
     }
 
@@ -20,50 +17,59 @@ export class PFAnimation {
         for (const key in this._parameters) {
             const parameter = this._parameters[key];
 
-            // time < startTime of first segment
-            if (time < parameter[0].startTime) {
-                values[key] = parameter[0].startValue;
-                if (parameter[0].startSpeed) {
-                    values[key] -= parameter[0].startSpeed * (parameter[0].startTime - time);
+            // time <= startTime of first segment
+            if (time <= parameter[0].time) {
+                values[key] = parameter[0].value;
+                if (parameter[0].speed) {
+                    values[key] -= parameter[0].speed * (parameter[0].time - time);
                 }
                 continue;
             }
 
-            // time > endTime of last segment
+            // time >= endTime of last segment
             const lastSegment = parameter[parameter.length - 1];
-            if (time > lastSegment.endTime) {
-                values[key] = lastSegment.endValue;
-                if (lastSegment.endSpeed) {
-                    values[key] += lastSegment.endSpeed * (time - lastSegment.endTime);
+            if (time >= lastSegment.time) {
+                values[key] = lastSegment.value;
+                if (lastSegment.speed) {
+                    values[key] += lastSegment.speed * (time - lastSegment.time);
                 }
                 continue;
             }
 
             // time is between two segments
-            const segment = parameter.find((p: ParameterSegment) => time >= p.startTime && time <= p.endTime);
-            if (segment) {
-                const duration = segment.endTime - segment.startTime;
-                const t = (time - segment.startTime) / duration;
+            // find last element that has item.time <= time
+            const keyframeBefore = parameter.filter((p: ParameterKeyframe) => p.time <= time).pop();
+            const keyframeAfter = parameter.filter((p: ParameterKeyframe) => p.time > time).shift();
 
-                const p0 = segment.startValue;
-                const p3 = segment.endValue;
-                const p1 = segment.startSpeed !== undefined
-                    ? p0 + segment.startSpeed * duration / 3
-                    : p0 + (p3 - p0) / 3;
-                const p2 = segment.endSpeed !== undefined
-                    ? p3 - segment.endSpeed * duration / 3
-                    : p3 - (p3 - p0) / 3;
-
-                const oneMinusT = 1 - t;
-                values[key] = p0 * oneMinusT * oneMinusT * oneMinusT +
-                    p1 * 3 * t * oneMinusT * oneMinusT +
-                    p2 * 3 * t * t * oneMinusT +
-                    p3 * t * t * t;
+            if (!keyframeBefore || !keyframeAfter) {
+                // this shouldn't happen if the keyframes are sorted by time
+                values[key] = 0;
                 continue;
             }
 
-            // no segments or the segments are not ordered properly. either is illegal but just return 0
-            values[key] = 0;
+            const t0 = keyframeBefore.time;
+            const t1 = keyframeAfter.time;
+            const duration = t1 - t0;
+            const t = (time - t0) / duration;
+            const v0 = keyframeBefore.value;
+            const v1 = keyframeAfter.value;
+            const d0 = keyframeBefore.speed;
+            const d1 = keyframeAfter.speed;
+
+            const p0 = v0;
+            const p3 = v1;
+            const p1 = d0 !== undefined
+                ? v0 + d0 * duration / 3
+                : v0 + (v1 - v0) / 3;
+            const p2 = d1 !== undefined
+                ? v1 - d1 * duration / 3
+                : v1 - (v1 - v0) / 3;
+
+            const oneMinusT = 1 - t;
+            values[key] = p0 * oneMinusT * oneMinusT * oneMinusT +
+                p1 * 3 * t * oneMinusT * oneMinusT +
+                p2 * 3 * t * t * oneMinusT +
+                p3 * t * t * t;
         }
         return values;
     }
